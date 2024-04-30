321447db3c236806da65da33c88a3077f60ff62e
dungeonmaster161 <uttam.shikhar16@gmail.com>
2024-04-30 21:58:33 +0530

    length of linkedlist function is added

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,66 +1,3 @@
-// class SinglyLinkedList{
-//     private ListNode head;
-//     private static class ListNode{
-//         private int data;
-//         private ListNode next;
-        
-//         public ListNode(int data){
-//             this.data = data;
-//             this.next = null;
-//         }
-//     }
-
-//     public void display(){
-//         ListNode current = head;
-//         while (current!=null) {
-//             System.out.print(current.data+"->");
-//             current = current.next;
-//         }
-//        System.out.print("NULL");
-//     }
-
-//     public int lengthOfSinglyLinkedList(){
-//         ListNode current  = head;
-//         int length = 0;
-//         while (current!=null) {
-//             length++;
-//             current = current.next;
-//         }
-//         return length;
-//     }
-
-//     public void insetNodeAtBegining(int data){
-//         ListNode temp = new ListNode(data);
-//         temp.next = head;
-//         head = temp;
-//     }
-
-//     public void insetNodeAtEnd(int data){
-//         ListNode temp = new ListNode(data);
-//         ListNode current = head;
-//         while (current.next!=null) {
-//             current = current.next;
-//         }
-//         current.next = temp;
-//     }
-//     public static void main(String[] args) {
-//         SinglyLinkedList sll = new SinglyLinkedList();
-//         sll.head = new ListNode(1);
-//         ListNode  one = new ListNode(2);
-//         ListNode two = new ListNode(3);
-//         ListNode three = new ListNode(4);
-//         sll.head.next = one;
-//         one.next = two;
-//         two.next = three;
-//         sll.display();
-//         System.out.println("Length of singly Linked List is "+sll.lengthOfSinglyLinkedList());
-//         sll.insetNodeAtBegining(5);
-//         sll.insetNodeAtBegining(6);
-//         sll.insetNodeAtEnd(99);
-//         sll.display();
-//     }
-// }
-
 /**
  * SinglyLinkedList
  */
@@ -86,16 +23,27 @@ public class SinglyLinkedList {
         System.out.print("Null");
     }
 
+    public void lengthOfSignlyLinkedList(){
+        ListNode temp;
+        temp = head;
+        int countLength = 0;
+        while(temp!=null){
+            countLength++;
+            temp = temp.next;
+        }
+        System.out.println("Length of Linked List is : "+countLength);
+    }
+
     public static void main(String[] args){
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.head = new ListNode(1);
-        // ListNode head = new ListNode(1);
         ListNode two = new ListNode(2);
         ListNode three = new ListNode(3);
         ListNode four = new ListNode(4);
-       sll.head.next = two;
-       two.next = three;
-       three.next = four;
+        sll.head.next = two;
+        two.next = three;
+        three.next = four;
         sll.displayList();
+        sll.lengthOfSignlyLinkedList();
     }
 }
