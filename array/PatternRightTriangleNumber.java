10ef9d36b1aba318b591b49cb82b498bf7c9f26d
uttamshikhar16 <uttam.shikhar16@gmail.com>
2024-05-29 18:12:10 +0530

    new star pattern is added

---
+++ array/PatternRightTriangleNumber.java
@@ -0,0 +1,11 @@
+public class PatternRightTriangleNumber {
+    public static void main(String[] args) {
+        int  n = 5;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.println(j);
+            }
+            System.out.println("");
+        }
+    }
+}
