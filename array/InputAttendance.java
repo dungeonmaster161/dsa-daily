553ee8b37e8543657dfc424b8aa14d1cef44d347
dungeonmaster161 <uttam.shikhar16@gmail.com>
2024-06-13 22:39:28 +0530

    new practice problems are added

--- array/InputAttendance.java
+++ array/InputAttendance.java
@@ -92,3 +92,86 @@ public class Problems {
 		
 	}
 }
+
+
+package inputAttendance;
+
+public class InputAttendance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {1,1,1,1,0,0,0,0,1,1};
+		int minPercentageCriteria = 40;
+		float presentDays = 0;
+		float percentage ;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i] == 1) {
+				presentDays++;
+			}
+		}
+		percentage = ( presentDays/(arr.length-1))*100;
+		if(percentage > minPercentageCriteria) {
+			System.out.println("Yes "+percentage+"%");
+		}else {
+			System.out.println("No "+percentage);
+		}
+		
+	}
+
+}
+
+
+package arraySum;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		int arr[] = {5,6,3,2,1};
+		int target = 9;
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++)
+			{
+			
+			if( arr[i]+arr[j] == target ) {
+				System.out.print(i+","+j);
+				return;
+			}
+			}
+			
+		}
+   }
+}
+
+
+package tasks;
+import java.util.Scanner;
+public class Shopkeeper {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int startWithOne = 50;
+		int startWithTwo = 100;
+		int firstDigit = 0 , price = 0 ;
+		System.out.println("Enter product id");
+		int productId = sc.nextInt();
+		System.out.println("Enter No. of products");
+		int numberOfProducts = sc.nextInt();
+		while(productId!=0) {
+			productId = productId/10;
+			if(productId/10 == 0) {
+				firstDigit = productId;
+				break;
+			}
+		}
+		if(firstDigit == 1) {
+			price = numberOfProducts * 50;
+		}else if(firstDigit == 2) {
+			price = numberOfProducts * 100;
+		}
+		if(price>5000) {
+			price = price - ((price*10)/100);
+		}
+		System.out.println("Final price of product is "+price);
+	}
+
+}
