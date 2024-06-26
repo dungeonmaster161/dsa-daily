ffd1da7ff8155f8ff65d179a6d1c6e30ba520c15
uttamshikhar16 <uttam.shikhar16@gmail.com>
2024-06-26 22:35:55 +0530

    new question related to character shift is added

---
+++ string/StringSkip.java
@@ -0,0 +1,38 @@
+package practiceString;
+import java.util.Scanner;
+public class StringSkip {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int temp;
+			String s = "abcYZ";
+			String finalString = "";
+			System.out.println("Enter number of steps you want to shift");
+			byte n = sc.nextByte();
+			byte[] ba  = s.getBytes();
+			for(int b : ba) {
+				if(b>=65 && b<=90) {
+					b=b+n;
+					if(b>90) {
+					temp = b-90-1;
+					finalString = finalString+(char)(temp+65);
+					}else {
+						finalString = finalString+(char)b;	
+						System.out.println(finalString);
+						break;
+					}
+				}else{
+					b=b+n;
+					if(b>122) {
+					temp = b-122;
+					finalString =finalString+(char)(temp+97);
+					}else {
+						finalString =finalString+(char)b;	
+					}
+				}
+			}
+			System.out.println(finalString);
+			sc.close();
+	}
+
+}
